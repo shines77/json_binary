876b9fced31c5c695b859d2f6355103139e320b4
shines77 <gz_shines@msn.com>
2016-05-17 22:50:32 +0800

    First submit.
    Signed-off-by: shines77 <gz_shines@msn.com>

---
+++ src/json_binary_test/json_binary.h
@@ -0,0 +1,317 @@
+#pragma once
+
+#include <assert.h>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#define Z16     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+static const char escape_table[256] = {
+        // 0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
+          '0',  0,   0,   0,   0,   0,   0,   0,  'b', 't', 'n',   0, 'f', 'r',  0,   0,    // 00~0F
+           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    // 10~1F
+           0,   0,  '"',  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  '/',   // 20~2F
+         Z16,                                                                               // 30~3F
+         Z16,                                                                               // 40~4F
+           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  '\\', 0,   0,   0,    // 50~5F
+
+         Z16, Z16, Z16, Z16, Z16, Z16, Z16, Z16, Z16, Z16                                   // 60~FF
+};
+#undef Z16
+
+enum display_screen_t {
+    display_screen_off = false,
+    display_screen_on = true
+};
+
+template <bool display_on_screen = display_screen_off>
+class json_binary
+{
+public:
+    json_binary() = default;
+    ~json_binary() = default;
+
+    static std::size_t encode(char * json, std::size_t max_size, const char * data,
+        std::size_t length, bool fill_null = true) {
+        char * src = const_cast<char *>(data);
+        char * src_end = src + length;
+        char * dest = json;
+        char * dest_max = json + max_size;
+        if (fill_null)
+            dest_max -= 1;
+        // The json buffer size must be large than data length.
+        assert(max_size >= length);
+        // dest can not overflow dest_max forever.
+        while (src < src_end) {
+            char ch = *src;
+            char escape = escape_table[ch];
+            if (escape == 0) {
+                *dest++ = ch;
+                assert(dest <= dest_max);
+                src++;
+            }
+            else {
+                if (display_on_screen) {
+                    // '\\', '\"', '\/'
+                    if (ch > 32)
+                        *dest++ = '\\';
+                    *dest++ = '\\';
+                }
+                *dest++ = '\\';
+                *dest++ = escape;
+                assert(dest <= dest_max);
+                src++;
+            }
+        }
+        assert(dest != nullptr);
+        if (fill_null)
+            *dest++ = '\0';
+        assert(dest >= json);
+        return (dest - json);
+    }
+
+    static std::size_t encode_safe(char * json, std::size_t max_size, const char * data,
+        std::size_t length, bool fill_null = true) {
+        char * src = const_cast<char *>(data);
+        char * src_end = src + length;
+        char * dest = json;
+        char * dest_max = json + max_size;;
+        if (fill_null)
+            dest_max -= 1;
+        // The json buffer size can less than the data length.
+        while (src < src_end) {
+            char ch = *src;
+            char escape = escape_table[ch];
+            if (escape == 0) {
+                *dest++ = ch;
+                if (dest >= dest_max) {
+                    dest = dest_max;
+                    break;
+                }
+                src++;
+            }
+            else {
+                if (display_on_screen) {
+                    // '\\', '\"', '\/'
+                    if (ch > 32)
+                        *dest++ = '\\';
+                    *dest++ = '\\';
+                }
+                *dest++ = '\\';
+                *dest++ = escape;
+                if (dest >= dest_max) {
+                    dest = dest_max;
+                    break;
+                }
+                src++;
+            }
+        }
+        assert(dest != nullptr);
+        if (fill_null)
+            *dest++ = '\0';
+        assert(dest >= json);
+        return (dest - json);
+    }
+
+    static std::size_t encode(char * dest, std::size_t offset, std::size_t max_size,
+        const char * src, std::size_t length, bool fill_null = true) {
+        return json_binary::encode(dest + offset, max_size, src, length, fill_null);
+    }
+
+    static std::size_t encode(std::string::iterator & dest_begin, std::string::iterator & dest_end,
+        const std::string::const_iterator & src_begin, const std::string::const_iterator & src_end,
+        bool fill_null = true) {
+        std::string::const_iterator src;
+        std::string::iterator & dest = dest_begin;
+        // dest must be not overflow dest_end forever.
+        for (src = src_begin; src != src_end; ++src) {
+            char ch = *src;
+            char escape = escape_table[ch];
+            if (escape == 0) {
+                *dest++ = ch;
+                assert(dest != dest_end);
+                src++;
+            }
+            else {
+                if (display_on_screen) {
+                    // '\\', '\"', '\/'
+                    if (ch > 32)
+                        *dest++ = '\\';
+                    *dest++ = '\\';
+                }
+                *dest++ = '\\';
+                *dest++ = escape;
+                assert(dest != dest_end);
+                src++;
+            }
+        }
+        if (fill_null)
+            *dest++ = '\0';
+        return (dest - dest_begin);
+    }
+
+    static std::size_t encode(std::string & dest, std::size_t offset, std::size_t max_size,
+        const std::string & src, std::size_t length, bool fill_null = true) {
+        std::string::iterator & dest_begin = dest.begin() + offset;
+        std::string::iterator & dest_end = dest_begin + max_size;
+        std::string::const_iterator & src_begin = src.begin();
+        std::string::const_iterator & src_end = src_begin + length;
+        return json_binary::encode(dest_begin, dest_end, src_begin, src_end, fill_null);
+    }
+
+    static std::size_t encode(std::string & dest, std::size_t max_size,
+        const std::string & src, std::size_t length, bool fill_null = true) {
+        return json_binary::encode(dest, 0, max_size, src, length, fill_null);
+    }
+
+    static bool decode(std::string & content, const std::string & filename) {
+        return true;
+    }
+
+    static bool readFromFile(const std::string & filename, std::string & content, std::size_t & content_size) {
+        bool bSuccess = false;
+        std::ifstream ifs;
+        try {
+            if (filename.empty())
+                return false;
+            ifs.open(filename.c_str(), std::ios::in | std::ios_base::binary);
+            if (ifs.is_open()) {
+                ifs.seekg(0, std::ios_base::end);
+                std::streampos spos_end = ifs.tellg();
+                std::streamsize file_size = spos_end;
+                file_size += 1;
+                if ((std::streamsize)content.length() < file_size)
+                    content.resize(file_size);
+                if ((std::streamsize)content.length() < file_size) {
+                    return (file_size == 0);
+                }
+                static const std::streamsize kReadSize = 65536;
+                char * current = &content[0];
+                char * content_end = current + content.capacity();
+                std::streamsize read_total = 0;
+                ifs.seekg(0, std::ios_base::beg);
+                while (!ifs.eof()) {
+                    ifs.read(current, kReadSize);
+                    std::streamsize read_bytes = ifs.gcount();
+                    std::cout << "ReadBytes = " << read_bytes << std::endl;
+                    current += read_bytes;
+                    read_total += read_bytes;
+                }
+                content_size = read_total;
+                ifs.close();
+                bSuccess = true;
+            }
+        }
+        catch (const std::exception & e) {
+            std::cout << "json_binary::readFromFile() Exception: " << e.what() << std::endl;
+        }
+        return bSuccess;
+    }
+
+    static bool saveToFile(const std::string & filename, std::string & content, std::size_t content_size) {
+        bool bSuccess = false;
+        std::ofstream ofs;
+        try {
+            if (filename.empty())
+                return false;
+            ofs.open(filename.c_str(), std::ios::out | std::ios::trunc | std::ios_base::binary);
+            if (ofs.is_open()) {
+                static const std::streamsize kWriteSize = 65536;
+                std::streamsize content_left = content_size;
+                char * current = &content[0];
+                char * content_end = current + content.capacity();
+                std::streamsize write_bytes;
+                std::streamsize written_total = 0;
+                ofs.seekp(0, std::ios_base::beg);
+                while (!ofs.eof()) {
+                    if (content_left >= kWriteSize)
+                        write_bytes = kWriteSize;
+                    else
+                        write_bytes = content_left;
+                    ofs.write(current, write_bytes);
+                    ofs.seekp(0, std::ios_base::cur);
+                    std::streampos written_bytes = ofs.tellp();
+                    std::cout << "WrittenBytes = " << written_bytes << std::endl;
+                    current += written_bytes;
+                    written_total += written_bytes;
+                    content_left -= write_bytes;
+                    if (content_left <= 0)
+                        break;
+                }
+                ofs.close();
+                bSuccess = true;
+            }
+        }
+        catch (const std::exception & e) {
+            std::cout << "json_binary::saveToFile() Exception: " << e.what() << std::endl;
+        }
+        return bSuccess;
+    }
+
+    static bool encodeFromFile(const std::string & filename, std::string & str,
+        std::size_t & encode_size, bool add_quote = false) {
+        bool bSuccess = false;
+        std::ifstream ifs;
+        try {
+            if (filename.empty())
+                return false;
+            ifs.open(filename.c_str(), std::ios::in | std::ios_base::binary);
+            if (ifs.is_open()) {
+                ifs.seekg(0, std::ios_base::end);
+                std::streampos spos_end = ifs.tellg();
+                std::streamsize file_size = spos_end;
+                file_size += 1;
+                if ((std::streamsize)str.capacity() < file_size)
+                    str.reserve(file_size);
+                if ((std::streamsize)str.capacity() < file_size) {
+                    return (file_size == 0);
+                }
+                static const std::streamsize kReadSize = 65536;
+                std::string json_str;
+                std::size_t json_size;
+                if (display_on_screen)
+                    json_size = file_size * 4 + 1;
+                else
+                    json_size = file_size * 2 + 1;
+                if (add_quote)
+                    json_size += 3;
+                json_str.reserve(json_size);
+                char * current = &str[0];
+                char * content_end = current + str.capacity();
+                char * json_start = &json_str[0];
+                char * json_end = json_start + json_str.capacity();
+                char * json = json_start;
+                std::size_t encode_total = 0;
+                if (add_quote)
+                    *json++ = '\"';
+                ifs.seekg(0, std::ios_base::beg);
+                while (!ifs.eof()) {
+                    ifs.read(current, kReadSize);
+                    std::streamsize read_bytes = ifs.gcount();
+                    assert(json <= json_end);
+                    assert(current <= content_end);
+                    std::size_t encode_bytes = json_binary::encode(json, json_end - json, current, read_bytes, false);
+                    encode_total += encode_bytes;
+                    std::cout << "ReadBytes = " << read_bytes << ", EscapeBytes = " << encode_bytes << std::endl;
+                    current += read_bytes;
+                    json += encode_bytes;
+                    if (json > json_end)
+                        break;
+                }
+                if (add_quote) {
+                    *json++ = '\"';
+                    *json = '\0';
+                }
+                encode_size = json - json_start;
+                str.swap(json_str);
+                ifs.close();
+                bSuccess = true;
+            }
+        }
+        catch (const std::exception & e) {
+            std::cout << "json_binary::readFromFileAndEncode() Exception: " << e.what() << std::endl;
+        }
+        return bSuccess;
+    }
+};
---
+++ src/json_binary_test/json_binary_test.cpp
@@ -0,0 +1,144 @@
+// JSON simple example
+// This example does not handle errors.
+
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <iostream>
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/prettywriter.h" // for stringify JSON
+
+#include "json_binary.h"
+
+using namespace rapidjson;
+
+class Employee {
+private:
+    std::string name_;
+    unsigned age_;
+    bool married_;
+    std::string dependents_;
+    std::string file_content_;
+    std::size_t content_size_;
+
+public:
+    Employee(const std::string& name, unsigned age, bool married) : name_(name), age_(age),
+        married_(married), dependents_(""), file_content_(""), content_size_(0) {}
+    Employee(const Employee& rhs) : name_(rhs.name_), age_(rhs.age_),
+        married_(rhs.married_), dependents_(rhs.dependents_),
+        file_content_(rhs.file_content_), content_size_(rhs.content_size_) {
+        if (this != &rhs) {
+            //
+        }
+    }
+    virtual ~Employee() {};
+
+    Employee& operator =(const Employee& rhs) {
+        if (this != &rhs) {
+            this->name_ = rhs.name_;
+            this->age_ = rhs.age_;
+            this->married_ = rhs.married_;
+            this->dependents_ = rhs.dependents_;
+            this->file_content_ = rhs.file_content_;
+            this->content_size_ = rhs.content_size_;
+        }
+        return *this;
+    }
+
+    template <typename Writer>
+    void Serialize(Writer& writer) const {
+        writer.StartObject();
+
+        writer.String("name");
+        writer.String(name_.c_str());
+
+        writer.String("age");
+        writer.Uint(age_);
+
+        writer.String("married");
+        writer.Bool(married_);
+
+        writer.String("dependents");
+        writer.String(dependents_.c_str());
+
+        writer.String("binary");
+        //writer.String(buffer_);
+        //writer.String("Content\u5176\u5b9e\u8fd9\u4e2a\u4e0d\u53d7\u7406\u8d8a\u7ea7\u4e0a \u8bbf\u7684\u653f\u7b56\u771f\u7684\u633a\u597d\u7684\uff0c\u8fd9\u6837\u53ef\u4ee5\u5feb \u901f\u7d2f\u79ef\u5e95\u5c42\u8bbf\u6c11\u66b4\u623e\u7684\u813e\u6c14\u3001\u4f7f\u4e4b \u628a\u8bbf\u6c11\u53d8\u5f97\u8d8a\u6765\u8d8a\u60f3\u4f9d\u9760\u66b4\u529b\u6765\u89e3 \u51b3\u4ed6\u4eec\u7684\u95ee\u9898\uff0c\u4ece\u800c\u6d88\u706d\u771f\u6b63\u7684\u654c \u4eba\n\u002d\u0000");
+        //writer.String(R"(Content\u5176\u5b9e\u8fd9\u4e2a\u4e0d\u53d7\u7406\u8d8a\u7ea7\u4e0a \u8bbf\u7684\u653f\u7b56\u771f\u7684\u633a\u597d\u7684\uff0c\u8fd9\u6837\u53ef\u4ee5\u5feb \u901f\u7d2f\u79ef\u5e95\u5c42\u8bbf\u6c11\u66b4\u623e\u7684\u813e\u6c14\u3001\u4f7f\u4e4b \u628a\u8bbf\u6c11\u53d8\u5f97\u8d8a\u6765\u8d8a\u60f3\u4f9d\u9760\u66b4\u529b\u6765\u89e3 \u51b3\u4ed6\u4eec\u7684\u95ee\u9898\uff0c\u4ece\u800c\u6d88\u706d\u771f\u6b63\u7684\u654c \u4eba\n\u002d\u0000)");
+        if (content_size_ > 0)
+            writer.RawValue(file_content_.c_str(), content_size_, kStringType);
+        else
+            writer.String("[Empty]");
+
+        writer.EndObject();
+    }
+
+    bool ReadFromFile(const std::string & filename, bool add_quote = false) {
+        return json_binary<display_screen_on>::encodeFromFile(filename, file_content_, content_size_, add_quote);
+    }
+
+    bool SaveContentToFile(const std::string & filename) {
+        return json_binary<display_screen_on>::saveToFile(filename, file_content_, content_size_);
+    }
+};
+
+void test_serialize()
+{
+    std::vector<Employee> employees;
+
+    Employee milo("Milo YIP", 34, true);
+    milo.ReadFromFile("C:\\test_json_binary.bin", true);
+    milo.SaveContentToFile("C:\\test_json_binary_out.bin");
+
+    StringBuffer sb;
+    PrettyWriter<StringBuffer> writer(sb);
+
+    milo.Serialize(writer);
+
+    std::cout << std::endl;
+    ::puts(sb.GetString());
+    std::cout << std::endl;
+}
+
+void test_simple_dom()
+{
+    // 1. Parse a JSON string into DOM.
+    const char* json = "{\"project\": \"rapidjson\",\"stars\": 10, \"binary\": \"\"}";
+    Document d;
+    d.Parse(json);
+
+    // 2. Modify it by DOM.
+    Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    Value& bin = d["binary"];
+    std::string content;
+    std::size_t content_size = 0;
+    json_binary<display_screen_off>::encodeFromFile("C:\\test_json_binary.bin", content, content_size, false);
+    bin.SetString(content.c_str(), (rapidjson::SizeType)content_size);  
+
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    PrettyWriter<StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << std::endl;
+    std::cout << buffer.GetString() << std::endl;
+    std::cout << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+    test_simple_dom();
+    test_serialize();
+
+    ::system("pause");
+    return 0;
+}
