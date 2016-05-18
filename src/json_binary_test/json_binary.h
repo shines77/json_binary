fe0a472fec36f9b3b3f7a7b874ae9ade39d99025
shines77 <gz_shines@msn.com>
2016-05-18 12:06:53 +0800

    新增了Hex16(十六进制)序列化的代码;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/json_binary_test/json_binary.h
+++ src/json_binary_test/json_binary.h
@@ -16,6 +16,21 @@ static const char hex_l_table[16] = {
     'a', 'b', 'c', 'd', 'e', 'f'
 };
 
+#define F16 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+static const char hex_lookup_256[256] = {
+    // 0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
+     F16,                                                                               // 00~0F
+     F16,                                                                               // 10~1F
+     F16,                                                                               // 20~2F
+       0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  -1,  -1,  -1,  -1,  -1,  -1,    // 30~3F
+      10,  11,  12,  13,  14,  15,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,    // 40~4F
+     F16,                                                                               // 50~5F
+      10,  11,  12,  13,  14,  15,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,    // 60~6F
+     F16,                                                                               // 70~7F
+     F16, F16, F16, F16, F16, F16, F16, F16                                             // 80~FF
+};
+#undef  F16
+
 #define Z16 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 static const char escape_table[256] = {
     // 0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
@@ -25,15 +40,267 @@ static const char escape_table[256] = {
      Z16,                                                                               // 30~3F
      Z16,                                                                               // 40~4F
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  '\\', 0,   0,   0,    // 50~5F
-
-     Z16, Z16, Z16, Z16, Z16, Z16, Z16, Z16, Z16, Z16                                   // 60~FF
+     Z16,                                                                               // 60~6F
+     Z16,                                                                               // 70~7F
+     Z16, Z16, Z16, Z16, Z16, Z16, Z16, Z16                                             // 80~FF
 };
 #undef Z16
 
-struct binary_hex
+struct json_binary_hex
 {
-    binary_hex() = default;
-    ~binary_hex() = default;
+    json_binary_hex() = default;
+    ~json_binary_hex() = default;
+
+    static std::size_t get_encode_capacity(std::size_t data_length, bool add_quote = false) {
+        std::size_t capacity = data_length;
+        // One char convent to two Hex strings.
+        capacity *= 2;
+        // Add double quote and nullor format like: "xxxxxx" '\0'
+        if (add_quote)
+            capacity += 3;
+        else
+            capacity += 1;
+        return capacity;
+    }
+
+    static std::size_t get_decode_capacity(std::size_t data_length, bool add_quote = false) {
+        std::size_t capacity = data_length;
+        // One char convent to two Hex strings.
+        capacity = (capacity + 1) / 2;
+        // Add double quote and nullor format like: "xxxxxx" '\0'
+        if (add_quote)
+            capacity += 3;
+        else
+            capacity += 1;
+        return capacity;
+    }
+
+    // encode to upper HEX strings, like "AABBCCF0E5D9".
+    static std::streamsize encode(const char * src, std::size_t src_len, char * buffer,
+        std::size_t buf_size, bool fill_null = true) {
+        assert(src != nullptr);
+        assert(buffer != nullptr);
+        const char * src_end = src + src_len;
+        char * dest = buffer;
+        while (src < src_end) {
+            unsigned char c = (unsigned char)(*src);
+            *dest++ = hex_u_table[ c >> 4U   ];
+            *dest++ = hex_u_table[ c & 0x0FU ];
+            src++;
+        }
+        if (fill_null)
+            *dest = '\0';
+        assert(dest >= buffer);
+        return (dest - buffer);
+    }
+
+    // encode to lower HEX strings, like "aabbccf0e5d9".
+    static std::streamsize encode_lower(const char * src, std::size_t src_len, char * buffer,
+        std::size_t buf_size, bool fill_null = true) {
+        assert(src != nullptr);
+        assert(buffer != nullptr);
+        const char * src_end = src + src_len;
+        char * dest = buffer;
+        while (src < src_end) {
+            unsigned char c = (unsigned char)(*src);
+            *dest++ = hex_l_table[ c >> 4U   ];
+            *dest++ = hex_l_table[ c & 0x0FU ];
+            src++;
+        }
+        if (fill_null)
+            *dest = '\0';
+        assert(dest >= buffer);
+        return (dest - buffer);
+    }
+
+    // encode to upper HEX strings, like "AABBCCF0E5D9".
+    static std::streamsize encode_std(const char * src, std::size_t src_len, char * buffer,
+        std::size_t buf_size, bool fill_null = true) {
+        assert(src != nullptr);
+        assert(buffer != nullptr);
+        const char * src_end = src + src_len;
+        char * dest = buffer;
+        while (src < src_end) {
+            unsigned char c = (unsigned char)(*src);
+            unsigned int hex1, hex2;
+            hex1 = c >> 4;
+            hex2 = c & 0x0FU;
+            if (hex1 <= 9)
+                *dest = hex1 + '0';
+            else
+                *dest = hex1 - 10 + 'A';
+            if (hex2 <= 9)
+                *(dest + 1) = hex2 + '0';
+            else
+                *(dest + 1) = hex2 - 10 + 'A';
+            dest += 2;
+            src++;
+        }
+        if (fill_null)
+            *dest = '\0';
+        assert(dest >= buffer);
+        return (dest - buffer);
+    }
+
+    static std::streamsize decode(const char * src, std::size_t src_len, char * buffer,
+        std::size_t buf_size, bool fill_null = true) {
+        assert(src != nullptr);
+        assert(buffer != nullptr);
+        // src_len must be multiply of 2.
+        if ((src_len & 1UL) != 0) {
+            std::cout << "Error: binary_hex::decode(), src_len = " << src_len << " is not multiply of 2." << std::endl;
+            return -2;
+        }
+        src_len -= (src_len & 1U);
+        const char * src_end = src + src_len;
+        char * dest = buffer;
+        while (src < src_end) {
+            unsigned int hex, hex1 = 0, hex2 = 0;
+            unsigned int c1, c2;
+            // High Hex charactor
+            c1 = (unsigned int)(unsigned char)(*src);
+            if (c1 != -1) {
+                hex1 = hex_lookup_256[c1];
+            }
+            else {
+                // Error: include non-hex chars.
+                std::cout << "Error: binary_hex::decode(), high Hex include non-hex chars: [" << c1 << "]." << std::endl;
+                return -1;
+            }
+            // Low Hex charactor
+            c2 = (unsigned int)(unsigned char)(*(src + 1));
+            if (c1 != -1) {
+                hex2 = hex_lookup_256[c2];
+            }
+            else {
+                // Error: include non-hex chars.
+                std::cout << "Error: binary_hex::decode(), low Hex include non-hex chars: [" << c2 << "]." << std::endl;
+                return -1;
+            }
+            hex = (hex1 << 4) | hex2;
+            *dest++ = hex;
+            src += 2;
+        }
+        if (fill_null)
+            *dest = '\0';
+        assert(dest >= buffer);
+        return (dest - buffer);
+    }
+
+    static std::streamsize decode_std(const char * src, std::size_t src_len, char * buffer,
+        std::size_t buf_size, bool fill_null = true) {
+        assert(src != nullptr);
+        assert(buffer != nullptr);
+        // src_len must be multiply of 2.
+        if ((src_len & 1UL) != 0) {
+            std::cout << "Error: binary_hex::decode_std(), src_len = " << src_len << " is not multiply of 2." << std::endl;
+            return -2;
+        }
+        src_len -= (src_len & 1UL);
+        const char * src_end = src + src_len;
+        char * dest = buffer;
+        while (src < src_end) {
+            unsigned int hex, hex1 = 0, hex2 = 0;
+            unsigned int c1, c2;
+            // High Hex charactor
+            c1 = (unsigned int)(unsigned char)(*src);
+            if (c1 <= '9' && c1 >= '0') {
+                hex1 = c1 - '0';
+            }
+            else if (c1 <= 'F' && c1 >= 'A') {
+                hex1 = c1 - 'A';
+            }
+            else if (c1 <= 'f' && c1 >= 'a') {
+                hex1 = c1 - 'a';
+            }
+            else {
+                // Error: include non-hex chars.
+                std::cout << "Error: binary_hex::decode_std(), high Hex include non-hex chars: [" << c1 << "]." << std::endl;
+                return -1;
+            }
+            // Low Hex charactor
+            c2 = (unsigned int)(unsigned char)(*(src + 1));
+            if (c2 <= '9' && c2 >= '0') {
+                hex2 = c2 - '0';
+            }
+            else if (c2 <= 'F' && c2 >= 'A') {
+                hex2 = c2 - 'A';
+            }
+            else if (c2 <= 'f' && c2 >= 'a') {
+                hex2 = c2 - 'a';
+            }
+            else {
+                // Error: include non HEX chars.
+                std::cout << "Error: binary_hex::decode_std(), low Hex include non-hex chars: [" << c2 << "]." << std::endl;
+                return -1;
+            }
+            hex = (hex1 << 4) | hex2;
+            *dest++ = hex;
+            src += 2;
+        }
+        if (fill_null)
+            *dest = '\0';
+        assert(dest >= buffer);
+        return (dest - buffer);
+    }
+
+    static bool encodeFromFile(const std::string & filename, std::string & content,
+        std::size_t & encode_size, bool add_quote = false) {
+        bool bSuccess = false;
+        std::ifstream ifs;
+        try {
+            if (filename.empty())
+                return false;
+            ifs.open(filename.c_str(), std::ios::in | std::ios_base::binary);
+            if (ifs.is_open()) {
+                ifs.seekg(0, std::ios_base::end);
+                std::streampos spos_end = ifs.tellg();
+                std::streamsize file_size = spos_end;
+                
+                std::string json_bin;
+                std::size_t json_size;
+                json_size = file_size * 2 + 1;
+                if (add_quote)
+                    json_size += 3;
+                json_bin.reserve(json_size);
+
+                static const std::streamsize kReadSize = 65536;
+                char buffer[kReadSize];
+                char * json_start = &json_bin[0];
+                char * json_end = json_start + json_bin.capacity();
+                char * json = json_start;
+                std::streamsize json_capacity = (std::streamsize)json_bin.capacity();
+                std::size_t encode_total = 0;
+                if (add_quote)
+                    *json++ = '\"';
+                ifs.seekg(0, std::ios_base::beg);
+                while (!ifs.eof()) {
+                    ifs.read(buffer, kReadSize);
+                    std::streamsize read_bytes = ifs.gcount();
+                    assert(json <= json_end);
+                    std::size_t encode_bytes = json_binary_hex::encode(buffer, read_bytes, json, json_capacity, false);
+                    encode_total += encode_bytes;
+                    std::cout << "ReadBytes = " << read_bytes << ", EscapeBytes = " << encode_bytes << std::endl;
+                    json += encode_bytes;
+                    json_capacity -= encode_bytes;
+                    if (json_capacity <= 0)
+                        break;
+                }
+                if (add_quote) {
+                    *json++ = '\"';
+                    *json = '\0';
+                }
+                encode_size = json - json_start;
+                content.swap(json_bin);
+                ifs.close();
+                bSuccess = true;
+            }
+        }
+        catch (const std::exception & e) {
+            std::cout << "json_binary_hex::encodeFromFile() Exception: " << e.what() << std::endl;
+        }
+        return bSuccess;
+    }
 };
 
 enum double_escape_t {
@@ -41,7 +308,7 @@ enum double_escape_t {
     double_escape = true
 };
 
-template <bool DoubleEscape = single_escape>
+template <bool isDoubleEscape = single_escape>
 struct json_binary
 {
     json_binary() = default;
@@ -59,17 +326,17 @@ struct json_binary
         assert(max_size >= length);
         // dest can not overflow dest_max forever.
         while (src < src_end) {
-            char ch = *src;
-            char escape = escape_table[ch];
+            char c = *src;
+            char escape = escape_table[c];
             if (escape == 0) {
-                *dest++ = ch;
+                *dest++ = c;
                 assert(dest <= dest_max);
                 src++;
             }
             else {
-                if (DoubleEscape) {
+                if (isDoubleEscape) {
                     // '\\', '\"', '\/'
-                    if (ch > 32)
+                    if (c > 32)
                         *dest++ = '\\';
                     *dest++ = '\\';
                 }
@@ -81,7 +348,7 @@ struct json_binary
         }
         assert(dest != nullptr);
         if (fill_null)
-            *dest++ = '\0';
+            *dest = '\0';
         assert(dest >= json);
         return (dest - json);
     }
@@ -96,10 +363,10 @@ struct json_binary
             dest_max -= 1;
         // The json buffer size can less than the data length.
         while (src < src_end) {
-            char ch = *src;
-            char escape = escape_table[ch];
+            char c = *src;
+            char escape = escape_table[c];
             if (escape == 0) {
-                *dest++ = ch;
+                *dest++ = c;
                 if (dest >= dest_max) {
                     dest = dest_max;
                     break;
@@ -107,9 +374,9 @@ struct json_binary
                 src++;
             }
             else {
-                if (DoubleEscape) {
+                if (isDoubleEscape) {
                     // '\\', '\"', '\/'
-                    if (ch > 32)
+                    if (c > 32)
                         *dest++ = '\\';
                     *dest++ = '\\';
                 }
@@ -124,7 +391,7 @@ struct json_binary
         }
         assert(dest != nullptr);
         if (fill_null)
-            *dest++ = '\0';
+            *dest = '\0';
         assert(dest >= json);
         return (dest - json);
     }
@@ -141,17 +408,17 @@ struct json_binary
         std::string::iterator & dest = dest_begin;
         // dest must be not overflow dest_end forever.
         for (src = src_begin; src != src_end; ++src) {
-            char ch = *src;
-            char escape = escape_table[ch];
+            char c = *src;
+            char escape = escape_table[c];
             if (escape == 0) {
-                *dest++ = ch;
+                *dest++ = c;
                 assert(dest != dest_end);
                 src++;
             }
             else {
-                if (DoubleEscape) {
+                if (isDoubleEscape) {
                     // '\\', '\"', '\/'
-                    if (ch > 32)
+                    if (c > 32)
                         *dest++ = '\\';
                     *dest++ = '\\';
                 }
@@ -162,7 +429,7 @@ struct json_binary
             }
         }
         if (fill_null)
-            *dest++ = '\0';
+            *dest = '\0';
         return (dest - dest_begin);
     }
 
@@ -275,42 +542,37 @@ struct json_binary
                 ifs.seekg(0, std::ios_base::end);
                 std::streampos spos_end = ifs.tellg();
                 std::streamsize file_size = spos_end;
-                file_size += 1;
-                if ((std::streamsize)content.capacity() < file_size)
-                    content.reserve(file_size);
-                if ((std::streamsize)content.capacity() < file_size) {
-                    return (file_size == 0);
-                }
-                static const std::streamsize kReadSize = 65536;
+
                 std::string json_bin;
                 std::size_t json_size;
-                if (DoubleEscape)
+                if (isDoubleEscape)
                     json_size = file_size * 4 + 1;
                 else
                     json_size = file_size * 2 + 1;
                 if (add_quote)
                     json_size += 3;
                 json_bin.reserve(json_size);
-                char * current = &content[0];
-                char * content_end = current + content.capacity();
+
+                static const std::streamsize kReadSize = 65536;
+                char buffer[kReadSize];
                 char * json_start = &json_bin[0];
                 char * json_end = json_start + json_bin.capacity();
                 char * json = json_start;
+                std::streamsize json_capacity = (std::streamsize)json_bin.capacity();
                 std::size_t encode_total = 0;
                 if (add_quote)
                     *json++ = '\"';
                 ifs.seekg(0, std::ios_base::beg);
                 while (!ifs.eof()) {
-                    ifs.read(current, kReadSize);
+                    ifs.read(buffer, kReadSize);
                     std::streamsize read_bytes = ifs.gcount();
                     assert(json <= json_end);
-                    assert(current <= content_end);
-                    std::size_t encode_bytes = json_binary::encode(json, json_end - json, current, read_bytes, false);
+                    std::size_t encode_bytes = json_binary::encode(json, json_capacity, buffer, read_bytes, false);
                     encode_total += encode_bytes;
                     std::cout << "ReadBytes = " << read_bytes << ", EscapeBytes = " << encode_bytes << std::endl;
-                    current += read_bytes;
                     json += encode_bytes;
-                    if (json > json_end)
+                    json_capacity -= encode_bytes;
+                    if (json_capacity <= 0)
                         break;
                 }
                 if (add_quote) {
@@ -324,7 +586,7 @@ struct json_binary
             }
         }
         catch (const std::exception & e) {
-            std::cout << "json_binary::readFromFileAndEncode() Exception: " << e.what() << std::endl;
+            std::cout << "json_binary::encodeFromFile() Exception: " << e.what() << std::endl;
         }
         return bSuccess;
     }
--- src/json_binary_test/json_binary_test.cpp
+++ src/json_binary_test/json_binary_test.cpp
@@ -67,7 +67,7 @@ public:
         writer.String("dependents");
         writer.String(dependents_.c_str());
 
-        writer.String("binary");
+        writer.String("binary-hex");
         //writer.String(buffer_);
         //writer.String("Content\u5176\u5b9e\u8fd9\u4e2a\u4e0d\u53d7\u7406\u8d8a\u7ea7\u4e0a \u8bbf\u7684\u653f\u7b56\u771f\u7684\u633a\u597d\u7684\uff0c\u8fd9\u6837\u53ef\u4ee5\u5feb \u901f\u7d2f\u79ef\u5e95\u5c42\u8bbf\u6c11\u66b4\u623e\u7684\u813e\u6c14\u3001\u4f7f\u4e4b \u628a\u8bbf\u6c11\u53d8\u5f97\u8d8a\u6765\u8d8a\u60f3\u4f9d\u9760\u66b4\u529b\u6765\u89e3 \u51b3\u4ed6\u4eec\u7684\u95ee\u9898\uff0c\u4ece\u800c\u6d88\u706d\u771f\u6b63\u7684\u654c \u4eba\n\u002d\u0000");
         //writer.String(R"(Content\u5176\u5b9e\u8fd9\u4e2a\u4e0d\u53d7\u7406\u8d8a\u7ea7\u4e0a \u8bbf\u7684\u653f\u7b56\u771f\u7684\u633a\u597d\u7684\uff0c\u8fd9\u6837\u53ef\u4ee5\u5feb \u901f\u7d2f\u79ef\u5e95\u5c42\u8bbf\u6c11\u66b4\u623e\u7684\u813e\u6c14\u3001\u4f7f\u4e4b \u628a\u8bbf\u6c11\u53d8\u5f97\u8d8a\u6765\u8d8a\u60f3\u4f9d\u9760\u66b4\u529b\u6765\u89e3 \u51b3\u4ed6\u4eec\u7684\u95ee\u9898\uff0c\u4ece\u800c\u6d88\u706d\u771f\u6b63\u7684\u654c \u4eba\n\u002d\u0000)");
@@ -79,8 +79,12 @@ public:
         writer.EndObject();
     }
 
-    bool ReadFromFile(const std::string & filename, bool add_quote = false) {
+    bool EncodeFromFile(const std::string & filename, bool add_quote = false) {
         return json_binary<double_escape>::encodeFromFile(filename, file_content_, content_size_, add_quote);
+    }
+
+    bool EncodeHexFromFile(const std::string & filename, bool add_quote = false) {
+        return json_binary_hex::encodeFromFile(filename, file_content_, content_size_, add_quote);
     }
 
     bool SaveContentToFile(const std::string & filename) {
@@ -88,28 +92,10 @@ public:
     }
 };
 
-void test_serialize()
-{
-    std::vector<Employee> employees;
-
-    Employee milo("Milo YIP", 34, true);
-    milo.ReadFromFile("C:\\test_json_binary.bin", true);
-    milo.SaveContentToFile("C:\\test_json_binary_out.bin");
-
-    StringBuffer sb;
-    PrettyWriter<StringBuffer> writer(sb);
-
-    milo.Serialize(writer);
-
-    std::cout << std::endl;
-    ::puts(sb.GetString());
-    std::cout << std::endl;
-}
-
 void test_simple_dom()
 {
     // 1. Parse a JSON string into DOM.
-    const char* json = "{\"project\": \"rapidjson\",\"stars\": 10, \"binary\": \"\"}";
+    const char* json = "{\"project\": \"rapidjson\",\"stars\": 10, \"binary-hex\": \"\"}";
     Document doc;
     doc.Parse(json);
 
@@ -117,7 +103,7 @@ void test_simple_dom()
     Value& s = doc["stars"];
     s.SetInt(s.GetInt() + 1);
 
-    Value& bin = doc["binary"];
+    Value& bin = doc["binary-hex"];
     std::string content;
     std::size_t content_size = 0;
     json_binary<single_escape>::encodeFromFile("C:\\test_json_binary.bin", content, content_size, false);
@@ -134,10 +120,77 @@ void test_simple_dom()
     std::cout << std::endl;
 }
 
+void test_serialize()
+{
+    std::vector<Employee> employees;
+
+    Employee milo("Milo YIP", 34, true);
+    milo.EncodeFromFile("C:\\test_json_binary.bin", true);
+    milo.SaveContentToFile("C:\\test_json_binary_out.bin");
+
+    StringBuffer sb;
+    PrettyWriter<StringBuffer> writer(sb);
+
+    milo.Serialize(writer);
+
+    std::cout << std::endl;
+    ::puts(sb.GetString());
+    std::cout << std::endl;
+}
+
+void test_simple_dom_hex()
+{
+    // 1. Parse a JSON string into DOM.
+    const char* json = "{\"project\": \"rapidjson\",\"stars\": 10, \"binary-hex\": \"\"}";
+    Document doc;
+    doc.Parse(json);
+
+    // 2. Modify it by DOM.
+    Value& s = doc["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    Value& bin = doc["binary-hex"];
+    std::string content;
+    std::size_t content_size = 0;
+    json_binary_hex::encodeFromFile("C:\\test_json_binary.bin", content, content_size, false);
+    bin.SetString(content.c_str(), (rapidjson::SizeType)content_size);  
+
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    PrettyWriter<StringBuffer> writer(buffer);
+    doc.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << std::endl;
+    std::cout << buffer.GetString() << std::endl;
+    std::cout << std::endl;
+}
+
+void test_serialize_hex()
+{
+    std::vector<Employee> employees;
+
+    Employee milo("Milo YIP", 34, true);
+    milo.EncodeHexFromFile("C:\\test_json_binary.bin", true);
+    milo.SaveContentToFile("C:\\test_json_binary_hex_out.bin");
+
+    StringBuffer sb;
+    PrettyWriter<StringBuffer> writer(sb);
+
+    milo.Serialize(writer);
+
+    std::cout << std::endl;
+    ::puts(sb.GetString());
+    std::cout << std::endl;
+}
+
 int main(int argc, char * argv[])
 {
     test_simple_dom();
     test_serialize();
+
+    test_simple_dom_hex();
+    test_serialize_hex();
 
     ::system("pause");
     return 0;
